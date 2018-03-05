0d526bd6086bc26570e335ac1af4eb5f6cda3035
J Slaton <jnthn.sltn.wrk@gmail.com>
2018-03-05 12:25:27 -0800

    Added map for outSig in main

--- src/main/main.ino
+++ src/main/main.ino
@@ -6,19 +6,20 @@
 #define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
 #endif
 
-#define SAMPLINGRATE 44100
+#define BUFFSIZE 100
 
 /////////////////////////////////////MIC/////////////////////////////////////
 int micPin = A0;
-double micRead;
-double outSig;
+int speakerPin = 9;
+short micRead;
+short outSig;
 
 ///////////////////////////////////SD Card///////////////////////////////////
 
-float inBuffer1[44100];   int inBuffCtr1;   int currIn;
-float inBuffer2[44100];   int inBuffCtr2; 
-float outBuffer1[44100];  int outBuffCtr1;  int currOut;
-float outBuffer2[44100];  int outBuffCtr2;
+short inBuffer1[BUFFSIZE];   int inBuffCtr1;   int currIn;
+short inBuffer2[BUFFSIZE];   int inBuffCtr2; 
+short outBuffer1[BUFFSIZE];  int outBuffCtr1;  int currOut;
+short outBuffer2[BUFFSIZE];  int outBuffCtr2;
 
 void setup() {
   // put your setup code here, to run once:
@@ -39,6 +40,8 @@ void setup() {
   outBuffCtr2 = 0;
   currIn = 1;
   currOut = 1;
+
+  pinMode(speakerPin, OUTPUT);
 }
 
 void loop() {
@@ -52,31 +55,32 @@ void loop() {
   ///////////////////////////////SPEAKERS////////////////////////////////
   outSig = micRead >> 2 - 1;
   if (outSig < 0) outSig = 0; //We don't want to deal with - #
-  PORTD = outSig;
+  int mappedSig = map(outSig, 0, 1023, 0, 5);
+  digitalWrite(speakerPin, mappedSig);
 }
 
 
-void fillInBuffer(float inSig) {
+void fillInBuffer(short inSig) {
 
   if (currIn == 1) {
     inBuffer1[inBuffCtr1] = inSig; //insert signal inside buffer
-    if (inBuffCtr1 == (SAMPLINGRATE - 1)) {
+    if (inBuffCtr1 == (BUFFSIZE - 1)) {
       inBuffCtr1 = 0; //reset the counter when full
       currIn = 2;     //select next buffer when full
     }
   }
   else if (currIn == 2) {
     inBuffer2[inBuffCtr2] = inSig;
-    if (inBuffCtr2 == (SAMPLINGRATE - 1)) {
+    if (inBuffCtr2 == (BUFFSIZE - 1)) {
       inBuffCtr2 = 0; //reset the counter when full
       currIn = 1;     //select next buffer when full
     }
   }
 }
 
-void fillOutBuffer(float * outSigArr) {
+void fillOutBuffer(short * outSigArr) {
 
-  for (int i = 0; i < SAMPLINGRATE; i++) {
+  for (int i = 0; i < BUFFSIZE; i++) {
     if (currOut == 1) {
       outBuffer1[i] = outSigArr[i];
     }
